837f2fd97a09bed43d71ea0d965f2518486f6936
Rob Boyer <rob_boyer@mac.com>
2025-09-07 18:00:06 -0700

    database wip

---
+++ My Code/TrackPointStore.h
@@ -0,0 +1,13 @@
+//
+//  TrackPointStore.h
+//  Ascent
+//
+//  Created by Rob Boyer on 9/6/25.
+//  Copyright © 2025 Montebello Software, LLC. All rights reserved.
+//
+
+#ifndef TrackPointStore_h
+#define TrackPointStore_h
+
+
+#endif /* TrackPointStore_h */

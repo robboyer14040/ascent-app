b4b61cd635271ccae02c9bbb4e94b3f453f3189a
Rob Boyer <rob_boyer@mac.com>
2025-09-14 13:06:21 -0700

    altitude profile tweaking

--- My Code/MiniProfileView.mm
+++ My Code/MiniProfileView.mm
@@ -85,7 +85,7 @@ static float calcY(float ymin, float ymax, float h, float v)
 					ymaxGR:(float)ymaxGR yminGR:(float)yminGR
 					  ymax:(float)ymax ymin:(float)ymin
 {
-	[[[Utils colorFromDefaults:RCBDefaultAltitudeColor] colorWithAlphaComponent:.2] set];
+	[[[Utils colorFromDefaults:RCBDefaultAltitudeColor] colorWithAlphaComponent:.5] set];
 	float h = vTickBounds.size.height;
 	NSRect r;
 	if (kVerticalLeft == axis)
@@ -144,8 +144,9 @@ static float calcY(float ymin, float ymax, float h, float v)
     
    bounds.origin.y += 14;
    bounds.size.height -= 14;
-   [tickFontAttrs setObject:[NSColor blueColor] forKey:NSForegroundColorAttributeName];
-   DUDrawTickMarks(bounds, (int)kVerticalLeft, 0.0, [Utils convertClimbValue:minalt], 
+   [tickFontAttrs setObject:[Utils colorFromDefaults:RCBDefaultAltitudeColor]
+                     forKey:NSForegroundColorAttributeName];
+   DUDrawTickMarks(bounds, (int)kVerticalLeft, 0.0, [Utils convertClimbValue:minalt],
 				   [Utils convertClimbValue:minalt+altdif], numVertPoints, tickFontAttrs, NO, 99999999999.0);
 
    return bounds;
@@ -331,6 +332,7 @@ static float calcY(float ymin, float ymax, float h, float v)
 
 
 
+#if 0
 - (void)drawRect:(NSRect)rect
 {
 	NSRect bounds = [self bounds];
@@ -527,6 +529,217 @@ static float calcY(float ymin, float ymax, float h, float v)
 		[s drawAtPoint:NSMakePoint(x,y) 
 		withAttributes:textFontAttrs];
 	}
+}
+#endif
+
+- (void)drawRect:(NSRect)rect
+{
+    NSRect bounds = [self bounds];
+    NSRect drawBounds = [self drawBounds];
+
+    [NSGraphicsContext saveGraphicsState];
+
+    // Background + border of the drawing region
+    [[NSColor colorNamed:@"BackgroundPrimary"] set];
+    [NSBezierPath fillRect:bounds];
+    [[[NSColor blackColor] colorWithAlphaComponent:0.6] set];
+    [NSBezierPath setDefaultLineWidth:1.0];
+    [NSBezierPath strokeRect:[self drawBounds]];
+
+    [NSGraphicsContext restoreGraphicsState];
+
+    if ((currentTrack != nil) && [currentTrack hasElevationData] && ([[currentTrack goodPoints] count] > 1)) {
+        BOOL isStatute = [Utils boolFromDefaults:RCBDefaultUnitsAreEnglishKey];
+        NSRect plotBounds = [self getPlotBounds];
+        lastTrack = currentTrack;
+        NSMutableArray *pts = [currentTrack goodPoints];
+
+        NSNumber *mxn, *mnn;
+        [Utils maxMinAlt:pts max:&mxn min:&mnn];
+
+        float maxAltNative = [Utils convertClimbValue:[mxn floatValue]];
+        float dmNative = [Utils convertClimbValue:[Utils floatFromDefaults:RCBDefaultMaxAltitude]];
+        if ((dmNative > 0) && (maxAltNative < dmNative)) {
+            maxAltNative = dmNative;
+        }
+        if (isStatute)
+            maxAltNative = (float)((((int)maxAltNative + 99) / 100) * 100);
+        else
+            maxAltNative = (float)((((int)maxAltNative + 19) / 20) * 20);
+
+        float minAltNative = [Utils convertClimbValue:[mnn floatValue]];
+        if (isStatute)
+            minAltNative = (float)((((int)minAltNative - 199) / 100) * 100);
+        else
+            minAltNative = (float)((((int)minAltNative - 39) / 20) * 20);
+
+        NSNumber *num = [pts valueForKeyPath:@"@max.distance"];
+        maxdist = [num floatValue];
+
+        float incr;
+        numVertTickPoints = AdjustRuler(10, minAltNative, maxAltNative, &incr);
+        float altDifNative = (numVertTickPoints) * incr;
+
+        minAltNative = maxAltNative - altDifNative;
+        if (isStatute) {
+            maxalt = maxAltNative;
+            minalt = minAltNative;
+            altdif = altDifNative;
+        } else {
+            maxalt = MetersToFeet(maxAltNative);
+            minalt = MetersToFeet(minAltNative);
+            altdif = MetersToFeet(altDifNative);
+        }
+
+        [dpath removeAllPoints];
+
+        const CGFloat w = plotBounds.size.width;
+        const CGFloat h = plotBounds.size.height;
+        const CGFloat x0 = plotBounds.origin.x;
+        const CGFloat y0 = plotBounds.origin.y;
+
+        numPts = (int)[pts count];
+        [plottedPoints removeAllObjects];
+
+        NSTimeInterval dur = 0.0;
+        if (numPts > 0) {
+            TrackPoint *lastPt = [pts objectAtIndex:(numPts - 1)];
+            if ([lastPt DATE_METHOD] > [currentTrack DURATION_METHOD]) {
+                dur = [currentTrack duration];
+            } else {
+                dur = [currentTrack DURATION_METHOD];
+            }
+        }
+
+        if ((numPts > 0) && (maxdist > 0.0) && (altdif > 0.0f) && (h > 1.0f)) {
+            [dpath setLineWidth:0.25];
+
+            NSPoint p;
+            TrackPoint *pt = [pts objectAtIndex:0];
+            p.x = x0;
+
+            float alt = [currentTrack firstValidAltitudeUsingGoodPoints:0];
+            if (alt == BAD_ALTITUDE) alt = 0.0f;
+            p.y = y0 + ((alt - minalt) * h) / altdif;
+
+            MapPoint *mpt0 = [[MapPoint alloc] initWithPoint:p time:[pt DATE_METHOD] speed:[pt speed]];
+            [plottedPoints addObject:mpt0];
+            [mpt0 release];
+
+            [dpath moveToPoint:p];
+
+            int i = 1;
+            float lastdist = 0.0f;
+            while (i < numPts) {
+                pt = [pts objectAtIndex:i];
+
+                alt = [currentTrack firstValidAltitudeUsingGoodPoints:i];
+                if (alt == BAD_ALTITUDE) alt = 0.0f;
+
+                float dist = [pt distance];
+                if (dist < lastdist) dist = lastdist;
+                lastdist = dist;
+
+                if (xAxisIsTime) {
+                    NSTimeInterval t = [pt DATE_METHOD];
+                    p.x = (dur > 0.0) ? (x0 + ((w * t) / dur)) : x0;
+                } else {
+                    p.x = x0 + ((w * dist) / maxdist);
+                }
+                p.y = y0 + (((alt - minalt) * h) / altdif);
+
+                MapPoint *mpt = [[MapPoint alloc] initWithPoint:p time:[pt DATE_METHOD] speed:[pt speed]];
+                [plottedPoints addObject:mpt];
+                [mpt release];
+
+                [dpath lineToPoint:p];
+                ++i;
+            }
+        }
+
+        NSRect tbounds = [self drawTickMarks:numVertTickPoints];
+
+        [self drawRangeIndicator:kAltitude
+                          bounds:tbounds
+                         xoffset:0
+                            axis:0
+                          ymaxGR:maxalt                 // FEET
+                          yminGR:minalt                 // FEET
+                            ymax:[mxn floatValue]       // FEET
+                            ymin:[mnn floatValue]];     // FEET
+
+        if ((numPts > 0) && ![dpath isEmpty]) {
+            // ---- New 3D-ish rendering ----
+
+            // 1) Build the closed fill path under the altitude polyline
+            NSBezierPath *fillPath = [dpath copy];
+            NSPoint cp = [dpath currentPoint];
+            // Drop straight down to the baseline, then back to the left base, then close.
+            [fillPath lineToPoint:NSMakePoint(cp.x, y0)];
+            [fillPath lineToPoint:NSMakePoint(x0, y0)];
+            [fillPath closePath];
+
+            // 2) Create a subtle vertical gradient based on RCBDefaultAltitudeColor
+            NSColor *base = [Utils colorFromDefaults:RCBDefaultAltitudeColor];
+            if (!base) base = [NSColor systemGreenColor];
+
+            // Near the line: almost the base color, just slightly brighter
+            NSColor *topColor =
+                [[base blendedColorWithFraction:0.25 ofColor:[NSColor whiteColor]] colorWithAlphaComponent:0.95];
+
+            // At the bottom: a gently darkened version of the base
+            NSColor *bottomColor =
+                [[base blendedColorWithFraction:0.20 ofColor:[NSColor blackColor]] colorWithAlphaComponent:0.35];
+
+            NSGradient *grad = [[[NSGradient alloc] initWithStartingColor:topColor
+                                                             endingColor:bottomColor] autorelease];
+
+            // Clip to the fill area so the gradient stays under the line only
+            [NSGraphicsContext saveGraphicsState];
+            [fillPath addClip];
+
+            // Draw vertical gradient: brighter near the top (under the line), darker toward the baseline
+            // Angle -90 draws from top -> bottom.
+            [grad drawInRect:NSMakeRect(x0, y0, w, h) angle:-90.0];
+
+            [NSGraphicsContext restoreGraphicsState];
+
+            [fillPath release];
+
+            // 3) Optional: a very soft shadow right under the line to give lift (super subtle)
+            NSShadow *softShadow = [[[NSShadow alloc] init] autorelease];
+            softShadow.shadowColor = [[NSColor blackColor] colorWithAlphaComponent:0.15];
+            softShadow.shadowBlurRadius = 1.5;
+            softShadow.shadowOffset = NSMakeSize(0.0, -1.0);
+
+            [NSGraphicsContext saveGraphicsState];
+            [softShadow set];
+
+            // 4) Stroke the altitude line in solid black for crisp definition
+            ///[[NSColor blackColor] set];
+            [[[Utils colorFromDefaults:RCBDefaultAltitudeColor] colorWithAlphaComponent:0.9] set];
+            [dpath setLineWidth:.3];
+            [dpath stroke];
+
+            [NSGraphicsContext restoreGraphicsState];
+        }
+
+        [self drawAnimationBitmap];
+        [self drawSelectedLap];
+        [self drawSelectedSplits];
+
+        [[NSColor blackColor] set];
+        [NSBezierPath setDefaultLineWidth:0.4];
+        [NSBezierPath strokeRect:NSInsetRect(bounds, -0.4, 0.4)];
+    } else {
+        NSString *s = currentTrack ? @"No Altitude Data" : @"No Activity Selected";
+        NSSize size = [s sizeWithAttributes:textFontAttrs];
+        CGFloat x = drawBounds.origin.x + drawBounds.size.width / 2.0 - size.width / 2.0;
+        CGFloat y = (drawBounds.size.height / 2.0) - (size.height / 2.0);
+        [textFontAttrs setObject:[[NSColor blackColor] colorWithAlphaComponent:0.20]
+                          forKey:NSForegroundColorAttributeName];
+        [s drawAtPoint:NSMakePoint(x, y) withAttributes:textFontAttrs];
+    }
 }
 
 
